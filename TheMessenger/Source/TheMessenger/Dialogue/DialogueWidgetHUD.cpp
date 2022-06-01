c20ed05b8e71d0c88b4fdfc717c688818dbd942a
NikodemHamrol <77978843+NikorolDev@users.noreply.github.com>
2022-06-01 14:44:10 +0100

    Created a dialogue widget HUD
    - Created a dialogue widget HUD so that dialogue text can be displayed onto the screen
    - Created a BP version of it for designing the visual aspect of the widget
    - Added functionality to display character name and dialogue text from the dialogue manager
    - Added a function to hide the dialogue widget, which will be when the dialogue sequence is inactive

---
+++ TheMessenger/Source/TheMessenger/Dialogue/DialogueWidgetHUD.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DialogueWidgetHUD.h"
+
+#include <Components/RichTextBlock.h>
+
+void UDialogueWidgetHUD::NativeConstruct()
+{
+	// Call the native construct from the UUserWidget.
+	Super::NativeConstruct();
+
+	// Hide the dialogue widget so it is not visible when the player starts the game.
+	HideDialogue();
+
+	UE_LOG( LogTemp, Display, TEXT( "[UDialogueWidgetHUD::NativeConstruct L.15] DIALOGUE WIDGET CONSTRUCTED" ) );
+}
+
+void UDialogueWidgetHUD::DisplayText( FString& rsCharacterName, FString& rsDialogueText )
+{
+	// Check if this widget is not displayed.
+	if( !m_bIsDialogueDisplayed )
+	{
+		// Set the dialogue text to be visible and the bool to be true so this code does not get called when the 
+		// dialogue sequence is still playing.
+		SetVisibility( ESlateVisibility::Visible );
+		m_bIsDialogueDisplayed = true;
+
+		UE_LOG( LogTemp, Display, TEXT( "[UDialogueWidgetHUD::DisplayText L.28] DIALOGUE WIDGET IS NOW VISIBLE" ) );
+	}
+
+	// Set the text of the dialogue with the character name.
+	// Concatenate the text so the character name can be manipulated via "DT_WidgetTextData", which holds how text should look like.
+	DialogueText->SetText( FText::FromString( "<Character>" + rsCharacterName + "</>: " + rsDialogueText ) );
+	UE_LOG( LogTemp, Display, TEXT( "[UDialogueWidgetHUD::DisplayText L.34] NEW DIALOGUE TEXT DISPLAYED" ) );
+}
+
+void UDialogueWidgetHUD::HideDialogue()
+{
+	// Set visibility of the dialogue to be hidden and set the boolean to be false, so it can be redisplayed.
+	SetVisibility( ESlateVisibility::Hidden );
+	m_bIsDialogueDisplayed = false;
+
+	UE_LOG( LogTemp, Display, TEXT( "[UDialogueWidgetHUD::HideDialogue L.43] DIALOGUE WIDGET IS NOW HIDDEN" ) );
+}
---
+++ TheMessenger/Source/TheMessenger/Dialogue/DialogueWidgetHUD.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DialogueWidgetHUD.generated.h"
+
+// Forward class declaration
+class URichTextBlock;
+
+//-----------------------------------------------------------------------------------------------------------------------------
+// Class Name			: UDialogueWidgetHUD
+// Author				: Nikodem Hamrol
+// Classes Inherited	: UUserWidget
+// Purpose				: This class will be responsible to display the dialogue text onto the screen. As well as 
+//						,	appearing when the dialogue sequence starts and hiding when the sequence finishes
+//-----------------------------------------------------------------------------------------------------------------------------
+UCLASS()
+class THEMESSENGER_API UDialogueWidgetHUD : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	// This will check if the dialogue is already displayed on the screen.
+	bool m_bIsDialogueDisplayed;
+
+	UPROPERTY( meta = ( BindWidget ) )
+		URichTextBlock* DialogueText;
+
+protected:
+	//-----------------------------------------------------------------------------------------------------------------------------
+	// Function Name	: NativeConstruct() override
+	// Author			: Unreal Engine 4
+	// Editors			: Nikodem Hamrol
+	// Purpose			: This is the constructor to the widget, which is similar to the "Event Construct" in Blueprint Widget.
+	//					,	It is used to hide itself when starting the game.
+	//-----------------------------------------------------------------------------------------------------------------------------
+	virtual void NativeConstruct() override;
+
+public:
+	//-----------------------------------------------------------------------------------------------------------------------------
+	// Function Name	: DisplayText( FString& rsCharacterName, FString& rsDialogueText )
+	// Author			: Nikodem Hamrol
+	// Purpose			: To set the widget to visible when the dialogue sequence starts and 
+	//					,	display the text that is passed from the dialogue manager onto the screen.
+	// Parameters		: rsCharacterName	- The name of the character that speaks.
+	//					, rsDialogueText	- The dialogue text that needs to be displayed.
+	//-----------------------------------------------------------------------------------------------------------------------------
+	void DisplayText( FString& psCharacterName, FString& rsDialogueText );
+
+	//-----------------------------------------------------------------------------------------------------------------------------
+	// Function Name	: HideDialogue()
+	// Author			: Nikodem Hamrol
+	// Purpose			: To hide the widget when the dialogue finishes
+	//-----------------------------------------------------------------------------------------------------------------------------
+	void HideDialogue();
+};
