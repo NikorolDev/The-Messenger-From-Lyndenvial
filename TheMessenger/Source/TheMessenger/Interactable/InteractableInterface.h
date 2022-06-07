053d17006c64950bf16086645d93e378a11ba9df
NikodemHamrol <77978843+NikorolDev@users.noreply.github.com>
2022-06-07 13:49:47 +0100

    Created an interface for all interactable classes
    - Created an interactable interface that will be used for all classes that will be interacted with
    - Added functionality for interaction
    - Added functionality when the player will focus on the interactable and not focused

---
+++ TheMessenger/Source/TheMessenger/Interactable/InteractableInterface.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "InteractableInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UInteractableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class THEMESSENGER_API IInteractableInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	//-----------------------------------------------------------------------------------------------------------------------------
+	// Function Name		: OnInteract( AActor* Caller )
+	// Author				: Nikodem Hamrol
+	// Parameters			: Caller - The actor who called the function
+	// Purpose				: Signature of the function which allows an interaction with another actor
+	//-----------------------------------------------------------------------------------------------------------------------------
+	UFUNCTION( Category = "Interaction", BlueprintNativeEvent, BlueprintCallable )
+		void OnInteract( AActor* Caller );
+
+	//-----------------------------------------------------------------------------------------------------------------------------
+	// Function Name		: OnFocus()
+	// Author				: Nikodem Hamrol
+	// Purpose				: Signature of the function which allows the actor to be focused
+	//-----------------------------------------------------------------------------------------------------------------------------
+	UFUNCTION( Category = "Interaction", BlueprintNativeEvent, BlueprintCallable )
+		void OnFocus();
+
+	//-----------------------------------------------------------------------------------------------------------------------------
+	// Function Name		: LostFocus()
+	// Author				: Nikodem Hamrol
+	// Purpose				: Signature of the function which allows an interaction with another actor
+	//-----------------------------------------------------------------------------------------------------------------------------
+	UFUNCTION( Category = "Interaction", BlueprintNativeEvent, BlueprintCallable )
+		void LostFocus();
+};
