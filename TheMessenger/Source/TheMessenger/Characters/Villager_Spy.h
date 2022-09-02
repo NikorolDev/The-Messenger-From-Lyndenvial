a1e00c5e7978940bc95fef18a11fd49352b3ad4e
NikodemHamrol <77978843+NikorolDev@users.noreply.github.com>
2022-09-02 20:41:08 +0100

    Created the spy

--- TheMessenger/Source/TheMessenger/Characters/Villager_Spy.h
+++ TheMessenger/Source/TheMessenger/Characters/Villager_Spy.h
@@ -6,6 +6,9 @@
 #include "Villager_Base.h"
 #include "Villager_Spy.generated.h"
 
+class ALevelSequenceActor;
+class ULevelSequencePlayer;
+
 /**
  * 
  */
@@ -13,5 +16,22 @@ UCLASS()
 class THEMESSENGER_API AVillager_Spy : public AVillager_Base
 {
 	GENERATED_BODY()
+
+private:
+	bool m_bHasSequencePlayed;
+
+	// The sequence player to play the final cutscene.
+	ULevelSequencePlayer* m_pcLevelSequencePlayer;
+
+	// The sequence actor that is in the level which holds the sequence to play.
+	UPROPERTY( Category = "Properties|Interaction Sequence", EditInstanceOnly, meta = ( DisplayName = "Sequence" ) )
+		ALevelSequenceActor* m_pcLevelSequenceToPlay;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	virtual void OnDialogueFinished() override;
 	
 };
